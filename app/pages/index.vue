620f09d8cd7692ffb7fa03a57494ce4671e7a1ee
justine <justinedayrit012002@gmail.com>
2026-02-09 18:26:02 +0800

    added row and column

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,8 +1,94 @@
 <template>
   <div>
-    <h1>welcome to my nuxt app</h1>
+    <h1>Welcome To my project</h1>
     <v-btn>submit</v-btn>
   </div>
+
+  <v-row>
+
+    <v-col cols="13" md="3">
+      <v-card color="blue" height="120px" > 
+  
+        <v-card-text>
+         <v-row>
+          <v-cols cols="6">
+           <v-icon size="80">mdi-account circle</v-icon>
+    </v-cols>
+    <v-col cols="50">
+      <v-row no-gutters>
+        <v-col cols="10" class="text-h6 text-right">student</v-col>
+        <v-col cols="10" class="text-h3 text-right"> $10</v-col>
+
+              </v-row>
+            </v-col>
+          </v-row>
+        </v-card-text>
+      </v-card >
+    </v-col>
+
+    <v-col cols="13" md="3">
+      <v-card color="red" height="120px">
+
+         <v-card-text>
+         <v-row>
+          <v-cols cols="6">
+           <v-icon size="80">mdi-account circle</v-icon>
+    </v-cols>
+    <v-col cols="50">
+      <v-row no-gutters>
+        <v-col cols="10" class="text-h6 text-right">student</v-col>
+        <v-col cols="10" class="text-h3 text-right"> $20</v-col>
+
+              </v-row>
+            </v-col>
+          </v-row>
+        </v-card-text>
+      </v-card>
+    </v-col>
+
+    <v-col cols="13" md="3">
+      <v-card color="green" height="120px">
+
+         <v-card-text>
+         <v-row>
+          <v-cols cols="6">
+           <v-icon size="80">mdi-account circle</v-icon>
+    </v-cols>
+    <v-col cols="50">
+      <v-row no-gutters>
+        <v-col cols="10" class="text-h6 text-right">student</v-col>
+        <v-col cols="10" class="text-h3 text-right"> $30</v-col>
+
+              </v-row>
+            </v-col>
+          </v-row>
+        </v-card-text>
+      </v-card>
+    </v-col>
+
+    <v-col cols="13" md="3">
+      <v-card color="yellow" height="120px">
+
+         <v-card-text>
+         <v-row>
+          <v-cols cols="6">
+           <v-icon size="80">mdi-account circle</v-icon>
+    </v-cols>
+    <v-col cols="50">
+      <v-row no-gutters>
+        <v-col cols="10" class="text-h6 text-right">student</v-col>
+        <v-col cols="10" class="text-h3 text-right"> $40</v-col>
+
+              </v-row>
+            </v-col>
+          </v-row>
+        </v-card-text>
+      </v-card>
+    </v-col>
+    
+  </v-row>
+
+  
 </template>
 
 <script lang="ts" setup>
